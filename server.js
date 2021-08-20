db306c08255b2f4abe5cd2772a73f06323f79b72
ga1i13o <48190199+ga1i13o@users.noreply.github.com>
2021-08-20 12:27:59 +0200

    bugfix

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const path = require('path');
 const express = require('express');
 
@@ -10,3 +12,5 @@ app.use(express.static("./client/build"));
 app.get('*', (req, res) => {
     res.redirect('index.html');
 });
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/`)); 
