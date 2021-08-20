ef238af2a9a1c4bafcf8a6cfbd1e4d76260f1ba6
ga1i13o <48190199+ga1i13o@users.noreply.github.com>
2021-08-20 12:18:19 +0200

    bugfix

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const path = require('path');
-const port = process.env.PORT || 3001;
+const express = require('express');
 
+const port = process.env.PORT || 3001;
 const app = express();
+
 app.use(express.json());
 app.use(express.static("./client/build"));
 
